f93a782d920943a8ffea9ef91fc4e70690aa8397
Pablo <patolluch@gmail.com>
2018-01-16 23:34:36 +0000

    cronfix

--- cron.php
+++ cron.php
@@ -12,11 +12,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
 function update() {
     $time = date("his");
     $sql = "INSERT INTO energyTest (human)
