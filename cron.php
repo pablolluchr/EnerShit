abdbb1b89078fcf813b77900505bd6f0663403e7
Pablo <patolluch@gmail.com>
2018-01-16 23:28:30 +0000

    cron

---
+++ cron.php
@@ -0,0 +1,37 @@
+
+<?php
+$servername = "localhost";
+$username = "noxiveco_enrshit";
+$password = "dQ/X92x^F4H;Si<@";
+$dbname = "noxiveco_enershit";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+function update() {
+    $time = date("his");
+    $sql = "INSERT INTO energyTest (human)
+    VALUES (time)";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+for ($i=0; $i < 59; $i++) {
+  update();
+  sleep(1);
+}
+
+$conn->close();
+?>
