46a42c179ea7a6a34e3885eb92eab37703caa157
felixxwu <felixxwu@gmail.com>
2018-01-17 00:19:54 +0000

    started on dbfunctions

--- modules/databaseFunctions.php
+++ modules/databaseFunctions.php
@@ -15,4 +15,13 @@ function databaseConnect() {
 	}
 }
 
+function sqlSelect() {
+	global $conn;
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+}
+
 ?>
