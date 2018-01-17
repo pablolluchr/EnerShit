5dec22527070eb46f974eb3c1fc5edd6cfb2a9af
felixxwu <felixxwu@gmail.com>
2018-01-17 00:46:44 +0000

    added sql insert, maybe

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "noxiveco_enrshit";
-$password = "dQ/X92x^F4H;Si<@";
-$dbname = "noxiveco_enershit";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+
+
+// $servername = "localhost";
+// $username = "noxiveco_enrshit";
+// $password = "dQ/X92x^F4H;Si<@";
+// $dbname = "noxiveco_enershit";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
 
 
 ?>
--- modules/databaseFunctions.php
+++ modules/databaseFunctions.php
@@ -7,6 +7,7 @@ function databaseConnect() {
 	$dbname = "noxiveco_enershit";
 
 	// Create connection
+	global $conn;
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	
 	// Check connection
@@ -15,12 +16,41 @@ function databaseConnect() {
 	}
 }
 
-function sqlSelect($table, $criteria) {
+function sqlSelect() {
 	global $conn;
 	if ($conn->query($sql) === TRUE) {
 		echo "New record created successfully";
 	} else {
 		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+}
+
+function sqlInsert() {
+	$args = func_get_args();
+	$table = $args[0];
+	$query = "INSERT INTO `" . $table . "` VALUES (";
+	$i = 0;
+	// from args 1 to the end of array
+	while ($args[++$i]) {
+		// if NULL, simply add NULL without quotes
+		if ($args[$i] == "NULL") {
+			$query = $query . "NULL";
+		// if not NULL, add the arg with quotes
+		} else {
+			$query = $query . "'" . $args[$i] . "'";
+		}
+		// if a next arg exists, then add a comma
+		if ($args[$i + 1]) {
+			$query = $query . ", ";
+		}
+	}
+	// after all args, close the bracket
+	$query = $query . ")";
+
+	global $conn;
+	// if the insert fails, show error message
+	if (!($conn->query($query))) {
+		die("SQL INSERT Error: " . $query . "<br>" . $conn->error);
 	}
 }
 
