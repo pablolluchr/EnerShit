5ab3d92ea8a736980638256d7ae390066d72478b
Pablo <patolluch@gmail.com>
2018-01-17 15:23:46 +0000

    cron

--- shit/cron.php
+++ shit/cron.php
@@ -1,5 +1,10 @@
 
 <?php
+
+//define("root", "");
+//include (root . "init.php");
+
+
 $servername = "localhost";
 $username = "noxiveco_enrshit";
 $password = "dQ/X92x^F4H;Si<@";
@@ -7,26 +12,53 @@ $dbname = "noxiveco_enershit";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
-    die("da yeah connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
-function update() {
-    $time = date("his");
-    $sql = "INSERT INTO energyTest
-    VALUES ('$time', NULL, NULL, NULL, NULL)";
-    global $conn;
-    if ($conn->query($sql) === TRUE) {
+function update(){
+  $sql = "INSERT INTO energy (human)
+  VALUES (123)";
+
+  if ($conn->query($sql) === TRUE) {
       echo "New record created successfully";
-    } else {
+  } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+  }
 }
+
 for ($i=0; $i < 59; $i++) {
   update();
   sleep(1);
 }
 
+
+
+
+
+
+/*
+databaseConnect();
+
+$allocation=sqlSelect('resourceAllocation','*',"username='test'",'username');
+$currentEnergy=sqlSelect('energy','*',"username='test'",'username');
+
+//updates each row in the energy table by adding the current energy and the allocation
+sqlUpdate('energy',"username='test'",'human',($allocation['human']+$currentEnergy['human']));
+function update() {
+    $allocation=sqlSelect('resourceAllocation','*',"username='test'",'username');
+    $currentEnergy=sqlSelect('energy','*',"username='test'",'username');
+
+    //updates each row in the energy table by adding the current energy and the allocation
+    sqlUpdate('energy',"username='test'",'human',($allocation['human']+$currentEnergy['human']));
+
+}
+for ($i=0; $i < 59; $i++) {
+  update();
+  sleep(1);
+}
+*/
 $conn->close();
 ?>
