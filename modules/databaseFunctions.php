5ff7a95a6f91b0b7e314c72f7b18769f5535be37
felixxwu <felixxwu@gmail.com>
2018-01-17 01:09:04 +0000

    added sql update

--- modules/databaseFunctions.php
+++ modules/databaseFunctions.php
@@ -67,4 +67,14 @@ function sqlDelete($table, $criteria) {
 	}
 }
 
+function sqlUpdate($table, $criteria, $column, $value) {
+	$query = "UPDATE `" . $table . "` SET `" . $column . "` = '" . $value . "' WHERE " . $criteria;
+
+	global $conn;
+	// if the update fails, show error message
+	if (!($conn->query($query))) {
+		die("SQL UPDATE Error: " . $query . "<br>" . $conn->error);
+	}
+}
+
 ?>
